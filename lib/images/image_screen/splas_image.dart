9ec4371bf67bdb1615f13469169e07a7d7f4f224
SALIM_KHAN <145958548+salimkhan23@users.noreply.github.com>
2023-10-10 19:47:16 +0600

    Update splas_image.dart

--- lib/images/image_screen/splas_image.dart
+++ lib/images/image_screen/splas_image.dart
@@ -1,3 +1,4 @@
-class MImagePath {
-  static const String main = 'assets/images/logos.png';
+ MImagePath {
+  static const String main = 'assets/images/logos.png',
+  static const String nav = 'assets/images/logo1.png',
 }
